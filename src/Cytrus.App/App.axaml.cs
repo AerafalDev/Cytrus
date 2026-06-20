aaed80fc2cf4ec87e96065928453af4fe71d1f72
Aerafal <jimmy.mll@hotmail.com>
2026-06-21 00:14:04 +0200

    feat: update App.axaml and App.axaml.cs for dark theme and services

--- src/Cytrus.App/App.axaml.cs
+++ src/Cytrus.App/App.axaml.cs
@@ -1,10 +1,19 @@
 using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Cytrus.App.ViewModels;
+using Cytrus.App.Views;
+using Cytrus.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Cytrus.App;
 
 public sealed class App : Application
 {
+    private ServiceProvider? _services;
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -12,5 +21,19 @@ public sealed class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        var desktop = (IClassicDesktopStyleApplicationLifetime)ApplicationLifetime!;
+
+        _services = new ServiceCollection()
+            .AddLogging(static builder => builder.ClearProviders().AddSerilog())
+            .AddCytrus()
+            .AddSingleton<MainViewModel>()
+            .BuildServiceProvider();
+
+        var viewModel = _services.GetRequiredService<MainViewModel>();
+
+        desktop.MainWindow = new MainView { DataContext = viewModel };
+        desktop.ShutdownRequested += (_, _) => _services?.Dispose();
+
+        Dispatcher.InvokeAsync(() => viewModel.RefreshIndexCommand.ExecuteAsync(null));
     }
 }
