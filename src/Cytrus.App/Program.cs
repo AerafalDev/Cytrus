2af24937a9d97aa37cf627b276f1ff7a893b6578
Aerafal <jimmy.mll@hotmail.com>
2026-06-21 00:13:58 +0200

    feat: configure Serilog for logging in the application

--- src/Cytrus.App/Program.cs
+++ src/Cytrus.App/Program.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia;
+﻿using System.Globalization;
+using Avalonia;
+using Serilog;
 
 namespace Cytrus.App;
 
@@ -7,6 +9,12 @@ public static class Program
     [STAThread]
     public static void Main(string[] args)
     {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .Enrich.FromLogContext()
+            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss}] {Level:u3}: {Message:lj}{NewLine}{Exception}", formatProvider: CultureInfo.InvariantCulture)
+            .CreateLogger();
+
         AppBuilder
             .Configure<App>()
             .UsePlatformDetect()
