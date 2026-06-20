b2f289b3893d41730094fe174af518e85c27a473
Aerafal <jimmy.mll@hotmail.com>
2026-06-20 22:29:20 +0200

    feat: implement FlatSharpManifestReader and IManifestReader interface

---
+++ src/Cytrus/Manifest/FlatSharpManifestReader.cs
@@ -0,0 +1,80 @@
+﻿using Cytrus.Hash;
+using Cytrus.Models;
+using FlatSharp;
+
+using Fb = Cytrus.Manifests;
+
+namespace Cytrus.Manifest;
+
+public sealed class FlatSharpManifestReader : IManifestReader
+{
+    public GameManifest Read(byte[] manifestBytes)
+    {
+        ArgumentNullException.ThrowIfNull(manifestBytes);
+
+        if (manifestBytes.Length is 0)
+            throw new ArgumentException("Manifest buffer is empty.", nameof(manifestBytes));
+
+        Fb.Manifest root;
+
+        try
+        {
+            root = Fb.Manifest.Serializer.Parse(new ArrayInputBuffer(manifestBytes));
+        }
+        catch (Exception ex)
+        {
+            throw new InvalidDataException("The manifest is not a valid FlatBuffers buffer.", ex);
+        }
+
+        var fbFragments = root.Fragments;
+
+        if (fbFragments is null || fbFragments.Count is 0)
+            return new GameManifest([]);
+
+        var fragments = fbFragments.Select(MapFragment).ToList();
+
+        return new GameManifest(fragments);
+    }
+
+    private static FragmentInfo MapFragment(Fb.Fragment fragment)
+    {
+        var files = MapList(fragment.Files, MapFile);
+        var bundles = MapList(fragment.Bundles, MapBundle);
+
+        return new FragmentInfo(fragment.Name ?? string.Empty, files, bundles);
+    }
+
+    private static FileEntry MapFile(Fb.File file)
+    {
+        var chunks = MapList(file.Chunks, MapChunk);
+
+        return new FileEntry(
+            Name: file.Name ?? string.Empty,
+            Size: file.Size,
+            Hash: HashId.FromMemory(file.Hash),
+            Chunks: chunks,
+            Executable: file.Executable,
+            Symlink: string.IsNullOrEmpty(file.Symlink) ? null : file.Symlink);
+    }
+
+    private static BundleInfo MapBundle(Fb.Bundle bundle)
+    {
+        return new BundleInfo(HashId.FromMemory(bundle.Hash), MapList(bundle.Chunks, MapChunk));
+    }
+
+    private static ChunkInfo MapChunk(Fb.Chunk chunk) =>
+        new(HashId.FromMemory(chunk.Hash), chunk.Size, chunk.Offset);
+
+    private static TOut[] MapList<TIn, TOut>(IList<TIn>? source, Func<TIn, TOut> map)
+    {
+        if (source is null || source.Count is 0)
+            return [];
+
+        var result = new TOut[source.Count];
+
+        for (var i = 0; i < source.Count; i++)
+            result[i] = map(source[i]);
+
+        return result;
+    }
+}
---
+++ src/Cytrus/Manifest/IManifestReader.cs
@@ -0,0 +1,8 @@
+﻿using Cytrus.Models;
+
+namespace Cytrus.Manifest;
+
+public interface IManifestReader
+{
+    GameManifest Read(byte[] manifestBytes);
+}
